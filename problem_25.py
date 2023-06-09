897a6c3ee6665fe44de94ffa766252bd69a06968
Md. Mamun <mkmamun031@gmail.com>
2023-06-10 01:01:29 +0600

    Solution to problem 25

---
+++ problem_25.py
@@ -0,0 +1,4 @@
+# Problem 25
+# Write A Python Program To Get Student Marks , If Marks Is Less Than 40, It Display "Fail" Otherwise "Pass"
+
+print("Fail" if int(input("Enter Your Marks: ")) < 40 else "Pass")
