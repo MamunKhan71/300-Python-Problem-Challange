9171a8335c02efa2b9f2a5b4efa75a8cec4355a0
Md. Mamun <mkmamun031@gmail.com>
2023-06-09 15:20:31 +0600

    Problem 15_1 Solution!

---
+++ problem_15_1.py
@@ -0,0 +1,9 @@
+# Problem 15_1
+# Write A Program To Get 6 Number In The TUPLE And Display All Number And Then Clear TUPLE And Then Display
+
+tpl = ('1', '2', '3')
+print(tpl)
+del tpl
+print(tpl)
+
+## Note: You cannot remove items in a tuple. You can only remove the complete tuple from the program ~ Mamun
