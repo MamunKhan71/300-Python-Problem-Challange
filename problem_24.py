91a7ee19ae5a850b08641dd41e5e0e86ce67b283
Md. Mamun <mkmamun031@gmail.com>
2023-06-10 00:57:41 +0600

    Solution to problem 24

---
+++ problem_24.py
@@ -0,0 +1,3 @@
+# Problem 24
+# Write A Python Program To Get Two Number And Operator From The User To Perform Arithmetic Operation
+print(eval(' '.join(map(str, input("Enter The Number and the operand (20 + 30): ").split()))))
