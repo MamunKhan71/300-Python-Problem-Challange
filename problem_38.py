69cba523af869f34b710e3032b075ece2ea3eb43
Md. Mamun <mkmamun031@gmail.com>
2023-06-12 10:08:08 +0600

    Minor Changes!

--- problem_38.py
+++ problem_38.py
@@ -1,6 +1,12 @@
 # Write A Python Program To Convert Radian To Degree
 # Starts Here:
 import math
+
 number = float(input("Enter Your Number in Radian: "))
-degree = float("{:.3f}".format(number * 180/math.pi))
+degree = float("{:.3f}".format(number * 180 / math.pi))
 print(f"Number in degree is : {degree}")
+
+# Degree to Radian
+# number = float(input("Enter Your Number in Degree: "))
+# rad = float("{:.3f}".format(number * math.pi/180))
+# print(f"Number in radian is : {rad}")
