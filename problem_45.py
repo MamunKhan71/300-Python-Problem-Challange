6f60ff1399a5a61e514f25312400a187ff545115
Md. Mamun <mkmamun031@gmail.com>
2023-06-13 22:42:04 +0600

    Code improvements!

--- problem_45.py
+++ problem_45.py
@@ -1,10 +1,5 @@
 # Problem 45
 # Write A Python Program To Display Number From 1 To 30, Only Odd Number
 print("Even\t\tOdd")
-counter = int(0)
 for nm in range(0, 32):
-    counter += 1
-    print(f"{nm}\t\t\t", end='') if nm % 2 == 0 else print(f'{nm}', end='')
-    if counter == 2:
-        print("\n")
-        counter = 0
+    print(f"{nm}\t\t\t", end='') if nm % 2 == 0 else print(f'{nm}')
