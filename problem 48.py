bf5f19956fb07639dad5c884b4f0bd8b9e672ca2
Md. Mamun <mkmamun031@gmail.com>
2023-06-14 23:36:15 +0600

    Solution to problem 48

--- problem 48.py
+++ problem 48.py
@@ -6,8 +6,9 @@ num2 = int(input("Enter B: "))
 print("Before Swapping ")
 print(f"Num A : {num1}\tNum B: {num2}")
 print("After Swapping ")
-temp = num1
-num1 = num2
-num2 = temp
+# temp = num1
+# num1 = num2
+# num2 = temp
+# Without temp:
+num1, num2 = num2, num1
 print(f"Num A : {num1}\tNum B: {num2}")
-
