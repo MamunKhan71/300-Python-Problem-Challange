8b93f260146519fecbedd07221ebe53a28117c36
Md. Mamun <mkmamun031@gmail.com>
2023-06-15 23:38:02 +0600

    Minor Changes

--- problem_51.py
+++ problem_51.py
@@ -1,8 +1,10 @@
 # Problem 51
 # Write Python Program To Find Number Of Day Between Two Dates
 from datetime import datetime
-dt = input("Enter the starting date (DD-MM-YY : ")
-dt2 = input("Enter the ending date (DD-MM-YY : ")
+dt = input("Enter the starting date (DD-MM-YY) : ")
+dt2 = input("Enter the ending date (DD-MM-YY) : ")
 newDate1 = datetime.strptime(dt, '%d-%m-%y')
 newDate2 = datetime.strptime(dt2, '%d-%m-%y')
 print(f"Total Days: {(newDate2-newDate1).days}")
+print(f"Total Minutes: {float((newDate2-newDate1).days)*24*60}")
+print(f"Total Seconds: {float((newDate2-newDate1).days)*24*60*60}")
