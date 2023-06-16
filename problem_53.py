393db189edc2de51dd1133ac0b0b16d7a4729fbe
Md. Mamun <mkmamun031@gmail.com>
2023-06-16 12:50:59 +0600

    Solution to problem 53

---
+++ problem_53.py
@@ -0,0 +1,3 @@
+# Problem 53
+# Write A Python Program To Get Age From The User In Year, And Convert Age In To Seconds
+print("Your age is: " + str(float(input("Enter Your Age in Years: "))*365*24*60*60) + " Seconds!")
