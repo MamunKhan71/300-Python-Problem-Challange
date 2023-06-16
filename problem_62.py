ca08db332c9cc4afba820b04e22a9110cfcb095b
Md. Mamun <mkmamun031@gmail.com>
2023-06-16 23:13:33 +0600

    Solution to problem 62

---
+++ problem_62.py
@@ -0,0 +1,21 @@
+# Problem 62
+"""
+Write A Python Program To Get Different Information Of Students From
+User And Display All The Student Information In This Order.
+Name        ----
+Father name ----
+Email       ----
+Age         ----
+Contact     ----
+"""
+name = input("Name: ")
+f_name = input("Father's Name : ")
+email = input("Email: ")
+age = input("Age: ")
+contact = input("Phone: ")
+
+print(f"Name: \t\t\t\t{name}\n"
+      f"Father Name: \t\t{f_name}\n"
+      f"Email: \t\t\t\t{email}\n"
+      f"Age: \t\t\t\t{age}\n"
+      f"Contact: \t\t\t{contact}")
