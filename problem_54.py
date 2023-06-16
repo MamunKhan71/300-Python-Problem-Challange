80276e97f2793df1fbcd0ac92dc8d329a0f88c11
Md. Mamun <mkmamun031@gmail.com>
2023-06-16 12:57:20 +0600

    Solution to problem 54

---
+++ problem_54.py
@@ -0,0 +1,10 @@
+# Problem 54
+# Write A Python Program To Get A Number From The User To Find Factorial Of That Number
+factorial = int(1)
+inp = int(input("Enter a number to find its factorial: "))
+if inp >=1:
+    for num in range(1, inp + 1):
+        factorial *= num
+else:
+    print("Please Insert a valid value..")
+print(factorial)
