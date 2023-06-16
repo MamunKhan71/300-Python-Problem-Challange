2c8021ced6d6f01e7dba75019e9884bcb2a7b251
Md. Mamun <mkmamun031@gmail.com>
2023-06-16 22:39:51 +0600

    Solution to problem 58

---
+++ problem_58.py
@@ -0,0 +1,5 @@
+# Problem 58
+# Write A Python Program To Get First And Last Name From The User And Display Full Name
+f_name = input("First Name: ")
+l_name = input("Last Name: ")
+print(f"Your Full Name is : {f_name.upper()} {l_name.upper()}")
