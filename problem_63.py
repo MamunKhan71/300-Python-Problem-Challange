fe4047505c062d8c3412c8aeba9d85795e01a85a
Md. Mamun <mkmamun031@gmail.com>
2023-06-16 23:18:22 +0600

    Solution to problem 63

---
+++ problem_63.py
@@ -0,0 +1,4 @@
+# Problem 63
+# Write A Python Program To Find Maximum Number From A List Using Function
+inpt = list(map(int, input("Enter Number Separated by Space: ").split(" ")))
+print(max(inpt))
