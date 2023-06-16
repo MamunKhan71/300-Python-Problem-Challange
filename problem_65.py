78b70a0d5a2294af48b778281b51371e7ae9498e
Md. Mamun <mkmamun031@gmail.com>
2023-06-17 00:28:40 +0600

    Solution to problem 65

---
+++ problem_65.py
@@ -0,0 +1,6 @@
+# Problem 65
+# Write A Python Program To Find Area And Perimeter Of A Square Using Function
+square_param = int(input("Enter the number: "))
+area = pow(square_param,2)
+perimeter = 4*square_param
+print(f"Area : \t\t\t{area}\nPerimeter : \t{perimeter}")
