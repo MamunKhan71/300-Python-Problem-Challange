23ac3909af94e9128858400483aacc46d09dba39
Md. Mamun <mkmamun031@gmail.com>
2023-06-18 00:10:45 +0600

    Solution to problem 91

---
+++ problem_91.py
@@ -0,0 +1,7 @@
+# Problem 91
+# Write A Python Program Find A Density Of An Object
+# Formula: d = m/v -> m = mass, v = volume
+mass = int(input("Mass: "))
+volume = int(input("Volume: "))
+density = float(mass/volume)
+print(f"Density: {density} g/cm3")
