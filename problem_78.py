73b1239e8f2fa32c1344891638971df12ff761da
Md. Mamun <mkmamun031@gmail.com>
2023-06-17 19:41:19 +0600

    Solution to problem 78

---
+++ problem_78.py
@@ -0,0 +1,5 @@
+# Problem 78
+# a Write A Python Program To Get A Number To Return Its Next And Previous Number
+
+inpt = int(input("Number: "))
+print(f"Next Number: {inpt+1}\nPrevious Number: {inpt-1}")
