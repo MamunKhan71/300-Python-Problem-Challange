8f3f23e7f9fdaaadca83e3d5f5de5f3359475d91
Md. Mamun <mkmamun031@gmail.com>
2023-06-17 19:50:56 +0600

    Solution to problem 81

---
+++ problem_81.py
@@ -0,0 +1,4 @@
+# Problem 81
+# Write A Python Program To Find Sum Of ISt Number And The Last Number From A List
+num_list = [1,24,3,4,776,432,12,45,5]
+print(f"Sum of first and last is : {num_list[0]+num_list[-1]}")
