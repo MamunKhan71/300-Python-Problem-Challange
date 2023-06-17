51965b57689c2f0e05dee119e9a5aec7b934bf98
Md. Mamun <mkmamun031@gmail.com>
2023-06-18 00:40:31 +0600

    Solution to problem 93

---
+++ problem_93.py
@@ -0,0 +1,9 @@
+# Problem 93
+# Write A Python Program To Find Acceleration Of An Object Having Velocity t Time
+# Formula : Velocity = distance/time
+
+distance = int(input("Enter the distance(meter): "))
+tim = int(input("Enter the time(second): "))
+velocity = float(distance/tim)
+print(f"Velocity: {velocity} m/s")
+
