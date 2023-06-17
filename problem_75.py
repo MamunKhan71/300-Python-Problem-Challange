9dbf331da211435e6324f0e57709ca85f9ad5c82
Md. Mamun <mkmamun031@gmail.com>
2023-06-17 19:33:11 +0600

    Solution to problem 75

---
+++ problem_75.py
@@ -0,0 +1,9 @@
+# Problem 75
+# Write A Python Program To Clone Or Copy Of a 10 Color List
+
+first_list = ["#dsfaf", "#sdae4", "#erhggh", "#dfsd45", "#sadf4", "#dgsd5", "#4terf", "#ffff", "#dsfsaf", "#dfggf"]
+second_list = first_list.copy()
+# Manual
+# for lst in first_list:
+#     second_list.append(lst)
+print(second_list)
