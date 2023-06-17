d07d3e8716fad9b31aedb1228e5514a614b5aa99
Md. Mamun <mkmamun031@gmail.com>
2023-06-17 19:39:15 +0600

    Solution to problem 7

---
+++ problem_77.py
@@ -0,0 +1,8 @@
+# Problem 77
+# Write A Python Program To Check If The First And Last Number Of A Tuple Is The Same Or Not
+
+num_tpl = (1, 2, 4, 5, 6, 7, 8, 1)
+if num_tpl[0] == num_tpl[-1]:
+    print("Same")
+else:
+    print("Not Same")
