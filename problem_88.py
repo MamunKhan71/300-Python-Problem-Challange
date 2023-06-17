4dd489d8759c1731a09ffdf934d54d9f42078e73
Md. Mamun <mkmamun031@gmail.com>
2023-06-17 23:31:54 +0600

    Solution to problem 88

---
+++ problem_88.py
@@ -0,0 +1,8 @@
+# Problem 88
+# Write A Python Program To_Get A Number From User, The System Should
+# Add Auto Increment To That Number. Half Of User Entered Number
+# Should Be Incremented
+
+number = float(input('Enter Number: '))
+autoIncrement = number + (number/2)
+print(autoIncrement)
