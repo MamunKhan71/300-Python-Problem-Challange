d506bf3b005066f1367c63df592db044a815cb42
Md. Mamun <mkmamun031@gmail.com>
2023-06-17 19:43:34 +0600

    Problem 79

---
+++ problem_79.py
@@ -0,0 +1,4 @@
+# Problem 79
+# Write A Python Program To Print First And Last Characters From A String
+userInput = input("Enter the character: ")
+print(f'First Character: {userInput[0]}\nLast Character: {userInput[-1]}')
