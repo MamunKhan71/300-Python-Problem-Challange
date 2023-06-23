316b5ef753fb55b2660a5936bc3f7eab956410a2
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-06-23 08:10:22 +0600

    Solution to problem 130

---
+++ Intermediate/problem_130.py
@@ -0,0 +1,17 @@
+# Problem 130
+# Write a Python Program to find a maximum key in a dictionary (having key
+# with number data type)
+
+color = {
+    1: "Red",
+    2: "Blue",
+    3: "Green",
+    4: "Yellow",
+    5: "Orange",
+    6: "Magenta",
+    7: "Cyan",
+    8: "Black",
+    9: "Purple",
+    10: "Gray",
+}
+print(max(color.keys()))
