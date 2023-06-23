5d12ebe77705bffc6383740da786355473c66e2e
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-06-23 08:02:46 +0600

    Solution to problem 129

---
+++ Intermediate/problem_129.py
@@ -0,0 +1,8 @@
+# Problem 129
+# Write a Numpy program to create a 2x2 identity matrix.
+import numpy as np
+
+arry = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(arry)
+idnt = np.identity(4)
+print(idnt)
