e2cbd38a9bf9b831acd887d1c73369df0dbe6293
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-06-25 11:18:59 +0600

    Solution to problem 143

---
+++ Intermediate/problem_143.py
@@ -0,0 +1,7 @@
+# Problem 143
+# Write a Python Numpy program to convert a given array into a list.
+import numpy as np
+
+two_array = np.array([5, 4, 3, 2, 1])
+array_list = two_array.tolist()
+print(array_list)
