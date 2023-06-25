43a9acafadf7406a4e6b97e179dd8fb33bd7e85f
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-06-25 09:58:20 +0600

    Solution to problem 137

---
+++ Intermediate/problem_137.py
@@ -0,0 +1,7 @@
+# Problem 137
+# Write a Python NumPy program to add two array.
+import numpy as np
+array_one = np.array([1, 2, 4, 5])
+array_two = np.array([3, 4, 5, 6])
+array_three = np.add(array_one, array_two)
+print(array_three)
