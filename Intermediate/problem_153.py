5ace4679006645077e2e3950093fef1681c9f736
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-06-26 07:25:21 +0600

    Solution to problem 153

---
+++ Intermediate/problem_153.py
@@ -0,0 +1,11 @@
+# Problem 153
+# Write a NumPy program to create a three dimension array with shape
+# (3,4,4) and set to a variable
+import numpy as np
+
+three_dim_array = np.array(
+    [[[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+     [[10, 11, 12], [13, 14, 15], [16, 17, 18]],
+     [[19, 20, 21, ], [22, 23, 24], [25, 26, 27]]]
+)
+print(three_dim_array)
