1413f9ab5e0352225ceacd6960271b7850180cb9
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-06-26 21:31:49 +0600

    Solution to problem 156

--- Intermediate/problem_155.py
+++ Intermediate/problem_155.py
@@ -3,11 +3,11 @@
 
 import numpy as np
 
-array = np.array([1,2,3,4,5])
+array = np.array([1, 2, 3, 4, 5])
 print(f"Array: {array}")
 numberReplacer = int(input("Enter the number you want to replace: "))
 
-for i,v in enumerate(array):
+for i, v in enumerate(array):
     if v == numberReplacer:
         array[i] = int(input("New Number: "))
     else:
---
+++ Intermediate/problem_156.py
@@ -0,0 +1,9 @@
+# Problem 156
+# Write a Numpy program to perform addition, subtraction, division and
+# multiplication on Array Element.
+
+import numpy as np
+
+array = np.array([1, 2, 3, 4, 5])
+inpt = input("Enter the equation: ")
+print(eval(inpt))
