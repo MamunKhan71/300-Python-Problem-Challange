093da52c621c1ecc412371640e06453b2d4fd108
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-06-27 11:38:42 +0600

    Solution to problem 160

---
+++ Intermediate/problem_160.py
@@ -0,0 +1,7 @@
+# Problem 160
+# Write a Python Pandas program to find transpose of array
+import numpy as np
+
+arr1 = np.array([[1, 2, 3], [4, 5, 6]])
+arr_transpose = np.transpose(arr1)
+print(arr_transpose)
