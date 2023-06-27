ae20632074ca9459244feb08729565c7b10c2449
Md. Mamun <82976724+MamunKhan71@users.noreply.github.com>
2023-06-27 09:49:12 +0600

    Solution to problem 158 and 159

--- Intermediate/problem_158.py
+++ Intermediate/problem_158.py
@@ -1,9 +1,8 @@
 # Problem 158
-# Write a Python Pandas program to convert a Numpy array to a Pandas series
+# Write a Python Program to Display element of list with their occurrence
 
-import numpy as np
-import pandas as pd
-
-arr = np.array([1, 2, 3, 4, 5])
-pd_srs = pd.Series(arr)
-print(pd_srs)
+lst = [1, 2, 4, 6, 7, 8, 5, 4, 2, 1, 2, 3, 4, 5, 6, 7, 8, 4, 2, 1, 4, 6, 9, 9]
+un_lst = list(set(lst))
+for i in un_lst:
+    count = lst.count(i)
+    print(f"{i} appeared {count} times!")
---
+++ Intermediate/problem_159.py
@@ -0,0 +1,9 @@
+# Problem 159
+# Write a Python Pandas program to convert a Numpy array to a Pandas series
+
+import numpy as np
+import pandas as pd
+
+arr = np.array([1, 2, 3, 4, 5])
+pd_srs = pd.Series(arr)
+print(pd_srs)
